9cc43aaf65ecfab8f8da7fd7a5d5d8b27ea4cfdd
Grow PLUS <mdmalikulmulkimohan@gmail.com>
2025-09-28 15:05:41 +0600

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,11 @@
+// Security keys
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+
+// Disable file editing
+define('DISALLOW_FILE_EDIT', true);
+
+// Limit post revisions
+define('WP_POST_REVISIONS', 5);
